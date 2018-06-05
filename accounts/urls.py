4c02c6466a7953432dc5d0ee758f5b32d75494e7
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-06-05 14:48:52 +0200

    svasta

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
 	path('',views.index, name='index'),
 	path('login/', login, {'template_name':'accounts/login.html'}),
-]
+	path('logout/', logout, {'template_name':'accounts/logout.html'}),
+	]
